2f4330eec90c05492f24dab4650a9c8b265b79a9
Yashdeep001 <112737611+Yashdeep001@users.noreply.github.com>
2023-04-27 23:16:31 -0600

    Add files via upload

---
+++ Data/Models/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Data.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [RegularExpression(@"^\+?\d{10,}$", ErrorMessage = "Invalid phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Department is required")]
+        public string Department { get; set; }
+
+        [Required(ErrorMessage = "Designation is required")]
+        public string Designation { get; set; }
+
+        public bool IsValid()
+        {
+            if (string.IsNullOrEmpty(Name))
+                return false;
+            if (string.IsNullOrEmpty(Email))
+                return false;
+            if (string.IsNullOrEmpty(Phone))
+                return false;
+            if (string.IsNullOrEmpty(Address))
+                return false;
+            if (string.IsNullOrEmpty(Designation))
+                return false;
+            if (string.IsNullOrEmpty(Department))
+                return false;
+            return true;
+        }
+    }
+}
